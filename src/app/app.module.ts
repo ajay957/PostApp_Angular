4d30e8d463ca69a179a139b99f9dbb2b434ebf28
Ajay957 <ajay74.aa82@gmail.com>
2022-11-25 15:36:55 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { Router, RouterModule, Routes } from '@angular/router';
 
+
+const myRoute:Routes=[
+
+  {
+  path:"",
+  component:AddPostComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
